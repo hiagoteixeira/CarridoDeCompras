3162f8e5b93e0083359873d1152bf550e3bdbc54
hiago <hiago.lst@gmail.com>
2021-06-29 17:09:34 -0300

    Extracting data from json provider, fixed

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,15 +1,13 @@
 import jquery, { data } from 'jquery'
 
 export function getProducts(params) {
-    const itens
-    $.ajax({
-        dataType: "json",
-        url: `/src/provider/${params}-10-reais.json`,
-        success: function (data) {
-            $(data).each(function (e) {
-                const item = ("img" = e.imageUrl, "name" = e.name, "price" = e.price);
-                itens.push(item);
-            })
-        }
+    const items
+    $.getJSON(`/src/provider/${params}-10-reais.json`, function (data) {
+        data.items.forEach(item => {
+            const row = { "img": item.imageUrl, "name": item.name, "price": item.price };
+            items.push(row);
+        });
     })
+
+    return items;
 }
